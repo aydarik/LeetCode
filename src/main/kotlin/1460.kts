010789daa9c331bc004096597c91ae4465c70aac
aydar <aydar.gumerbaev@friday.de>
2021-12-24 22:15:24 +0100

    Initial commit

---
+++ src/main/kotlin/1460.kts
@@ -0,0 +1,12 @@
+println(canBeEqual(intArrayOf(1, 4), intArrayOf(4, 1)))
+
+/**
+ * Problem 1460
+ * Make Two Arrays Equal by Reversing Sub-arrays
+ *
+ * Easy (594 / 102)
+ * @see <a href="https://leetcode.com/problems/make-two-arrays-equal-by-reversing-sub-arrays/">LeetCode</a>
+ */
+fun canBeEqual(target: IntArray, arr: IntArray): Boolean {
+    return false
+}
---
+++ src/main/kotlin/1542.kts
@@ -0,0 +1,41 @@
+println(longestAwesome("254534643564356523464"))
+
+/**
+ * Problem 1542
+ * Find Longest Awesome Substring
+ *
+ * Hard (454 / 9)
+ * @see <a href="https://leetcode.com/problems/find-longest-awesome-substring/">LeetCode</a>
+ */
+
+fun longestAwesome(s: String): Int {
+    return findPalindrome(s)?.length ?: 1
+}
+
+fun findPalindrome(s: String): String? {
+    for (i in s.indices) {
+        // TODO: optimize, LeetCode execution fails with "Time Limit Exceeded"
+        for (j in 0..i) {
+            val substring = s.substring(j, s.length - i + j)
+            if (isPalindrome(substring)) return substring
+        }
+    }
+    return null
+}
+
+fun isPalindrome(s: String): Boolean {
+    val map = mutableMapOf(
+        '0' to 0,
+        '1' to 0,
+        '2' to 0,
+        '3' to 0,
+        '4' to 0,
+        '5' to 0,
+        '6' to 0,
+        '7' to 0,
+        '8' to 0,
+        '9' to 0
+    )
+    s.toCharArray().forEach { map[it] = map[it]!!.plus(1) }
+    return map.values.count { it % 2 != 0 } <= 1
+}
